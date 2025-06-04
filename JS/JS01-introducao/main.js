7397b41564b0c7163c6231b7e31918d9c69730eb
rcjf1553u <rone.carlos.junior.fernandes@gmail.com>
2025-06-03 21:38:33 -0300

    DEV_WEBIOS_2025_01

---
+++ JS/JS01-introducao/main.js
@@ -0,0 +1,33 @@
+var idade
+//JavaScript
+console.log("Hello world") //apresentar uma mensagem
+
+console.clear() // limpar
+
+//variaveis 
+var numero1 = 192
+console.log(numero1)
+
+var num1 = 4
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 89 // iniciar a variavel em outro momento do codigo 
+console.log(idade)
+
+ano =1936
+const PI = 3.14
+
+
+//texto - strings 
+var nome = 'Pipoca doce real' 
+var nome2 = 'Pipoca doce smell'
+var nome3 = '1561665166161.56165651651' //numero
+
+// console.log(nome)
+// console.log(nome2)
+// console.log(nome3) 
+//pode fazer assim ou
+console.log(nome,nome2,nome3 )
+
