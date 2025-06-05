4663622afc42e30d4567c3eea336f4f61b017661
rcjf1553u <rone.carlos.junior.fernandes@gmail.com>
2025-06-04 21:32:19 -0300

    JS

--- JS/JS01-introducao/main.js
+++ JS/JS01-introducao/main.js
@@ -29,5 +29,36 @@ var nome3 = '1561665166161.56165651651' //numero
 // console.log(nome2)
 // console.log(nome3) 
 //pode fazer assim ou
-console.log(nome,nome2,nome3 )
+console.log(nome,nome2,nome3 ) 
 
+console.log('Olá' + 'pai') // concatenar
+console.log("Aluno" + nome + "e Aluno" + nome2 + 'tem um total de' + nome3 + "dinheiros") //Concatenar 2
+
+console.log(`Aluno ${nome} e Aluno ${nome2} Tem um total de ${nome3} dinheiros`) //Concatenar 3
+
+//operadores
+var valor1 = 5
+var valor2 = 5
+console.log(valor1 + valor2)
+console.log(valor1 - valor2)
+console.log(valor1 * valor2)
+console.log(valor1 / valor2)
+console.log(valor1 ** valor2)
+
+//operadores relacionais
+console.log(5 == 5)
+console.log(5 > 5)
+console.log(22 < 8)
+console.log(22 >=22)
+console.log(22 != 23)
+console.log('Ana' == 'ana')
+console.log(true === true) //bolean e o outro txt > string
+console.log(true != 'false') //true
+
+let temCamisa = false
+let temCracha = true
+console.log(temCamisa)
+console.log(TemCracha)
+console.log(temCamisa == true && temCracha == true) 
+
+console.log(temCamisa == true || temCracha == true) 
