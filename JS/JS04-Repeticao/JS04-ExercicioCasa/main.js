405a4369d551f136f6b3d3c784022a24245fb561
rcjf1553u <rone.carlos.junior.fernandes@gmail.com>
2025-06-12 20:58:02 -0300

    JS

---
+++ JS/JS04-Repeticao/JS04-ExercicioCasa/main.js
@@ -0,0 +1,16 @@
+let tabuada = 2;
+// let tabuada = 30,;
+
+if (tabuada === 2) {
+    console.log("Tabuada do 2:");
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+    }
+} else if (tabuada === 3) {
+    console.log("Tabuada do 3:");
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+    }
+} else {
+    console.log("Tabuada inválida.");
+}
