4a473eac420cf17d77468efff17221ec7de1231f
rcjf1553u <rone.carlos.junior.fernandes@gmail.com>
2025-06-12 20:49:55 -0300

    JS

--- JS/JS03-Condicional/JS03-ExercicioCasa/main.js
+++ JS/JS03-Condicional/JS03-ExercicioCasa/main.js
@@ -6,6 +6,4 @@ if (numero1 > numero2) {
     console.log("numero1 é maior que numero2");
 } else if (numero1 < numero2) {
     console.log("numero1 é menor que numero2");
-} else {
-    console.log("numero1 e numero2 são iguais");
-}
+} 
---
+++ JS/JS04-Repeticao/main.js
@@ -0,0 +1,16 @@
+
+
+let contador = 0
+//Enquanto a variavel contador for menor que 10 o while apresentara o valor da variavel do console 
+while(contador<10){
+    console.log(contador)
+    //contador = contador+1
+    contador +=1
+    contador++  
+}
+console.log("Finalizou o programa")
+
+//variavel inicial;condicao;incremento
+for (contador = 0; contador <=10; contador++){
+    console.log(contador)
+}
