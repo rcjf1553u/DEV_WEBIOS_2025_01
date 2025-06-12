50a187fbd7bfc112eaa7691d5e4b888b7bfc2cb8
rcjf1553u <rone.carlos.junior.fernandes@gmail.com>
2025-06-12 19:55:16 -0300

    JS

---
+++ JS/JS03-Condicional/JS03-ExercicioCasa/main.js
@@ -0,0 +1,11 @@
+let numero1 = 4;
+let numero2 = 2;
+
+// Verifica qual é maior e imprime o resultado
+if (numero1 > numero2) {
+    console.log("numero1 é maior que numero2");
+} else if (numero1 < numero2) {
+    console.log("numero1 é menor que numero2");
+} else {
+    console.log("numero1 e numero2 são iguais");
+}
